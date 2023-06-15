1ea76eff20da4f58e08da5b9a454f5413aef2c04
GuRuGu <peter.k.nets@gmail.com>
2023-06-15 12:19:23 +0300

    feat: define custom fonts and colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './theme';
+import theme from '@/src/theme';
+import { inter, montserrat } from '@/src/utils/fonts';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme} resetCSS>
+      <style jsx global>{`
+        :root {
+          --inter-font: ${inter.style.fontFamily};
+          --montserrat-font: ${montserrat.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />;
     </ChakraProvider>
   );
