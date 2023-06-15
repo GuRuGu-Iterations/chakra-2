a44d64a659376ddae072b386a7bd303e3ecc0def
GuRuGu <peter.k.nets@gmail.com>
2023-06-15 11:05:27 +0300

    feat: implement light-dark theme switching

--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -7,13 +7,18 @@ import {
   Heading,
   Text,
   Divider,
-  Flex,
   AspectRatio,
   Stack,
+  useColorMode,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import Skateboard from '@/src/assets/skateboard.png';
 
 const Cart = () => {
+  const { toggleColorMode } = useColorMode();
+  const bgColor = useColorModeValue('gray.50', 'whiteAlpha.50');
+  const secondaryTextColor = useColorModeValue('gray.600', 'gray.400');
+
   return (
     <VStack
       w='full'
@@ -21,13 +26,18 @@ const Cart = () => {
       p={10}
       spacing={6}
       alignItems='flex-start'
-      bg='gray.50'
+      bg={bgColor}
     >
       <VStack spacing={3} alignItems='flex-start'>
         <Heading size='2xl'>Your cart</Heading>
         <Text>
           If the price is too hard on your eyes,{' '}
-          <Button variant='link' colorScheme='black' fontWeight='bold'>
+          <Button
+            onClick={toggleColorMode}
+            variant='link'
+            colorScheme='black'
+            fontWeight='bold'
+          >
             try changing the theme.
           </Button>
         </Text>
@@ -45,28 +55,28 @@ const Cart = () => {
         >
           <VStack w='full' spacing={0} alignItems='flex-start'>
             <Heading size='md'>Penny board</Heading>
-            <Text>PNYCOMP27541</Text>
+            <Text color={secondaryTextColor}>PNYCOMP27541</Text>
           </VStack>
           <Heading size='sm'>$119.00</Heading>
         </Stack>
       </HStack>
       <VStack spacing={4} alignItems='stretch' w='full'>
         <HStack justifyContent='space-between'>
-          <Text color='gray.600'>Subtotal</Text>
+          <Text color={secondaryTextColor}>Subtotal</Text>
           <Heading size='sm'>$119.00</Heading>
         </HStack>
         <HStack justifyContent='space-between'>
-          <Text color='gray.600'>Shipping</Text>
+          <Text color={secondaryTextColor}>Shipping</Text>
           <Heading size='sm'>$19.99</Heading>
         </HStack>
         <HStack justifyContent='space-between'>
-          <Text color='gray.600'>Taxes (estimated)</Text>
+          <Text color={secondaryTextColor}>Taxes (estimated)</Text>
           <Heading size='sm'>$23.80</Heading>
         </HStack>
       </VStack>
       <Divider />
       <HStack justifyContent='space-between' w='full'>
-        <Text color='gray.600'>Total</Text>
+        <Text color={secondaryTextColor}>Total</Text>
         <Heading size='lg'>$162.79</Heading>
       </HStack>
     </VStack>
